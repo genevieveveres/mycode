87e4a2f32433bc7709de71194dbea42a236506c8
genevieveveres <genevieve.veres@gmail.com>
2023-10-30 20:54:02 +0000

    join and split

---
+++ challenge18.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+def main():
+    name_input = input("What's your name?\n>")
+    day_input = input("What day of the week is it?\n>")
+    print("Hello ",name_input,"! Happy ",day_input,"!",sep="")
+main()
