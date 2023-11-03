14e011fb7cfa5c2dc0936364151a7740261b0ded
genevieveveres <genevieveveres@gmail.com>
2023-11-03 15:33:20 +0000

    utilizing classes

--- oop2/dice_contest.py
+++ oop2/dice_contest.py
@@ -13,9 +13,14 @@ def main():
     # the player known as the loaded_dice
     loaded_dice = Cheat_Loaded_Dice()
 
+    mulligan = Cheat_Mulligan()
+    additional = Cheat_Additional_Die()
+
     # track scores for both players
     swapper_score = 0
     loaded_dice_score = 0
+    mulligan_score = 0
+    additional_score = 0
 
     # how many games we want to run
     number_of_games = 100000
@@ -27,9 +32,13 @@ def main():
     while game_number < number_of_games:
         swapper.roll()
         loaded_dice.roll()
+        mulligan.roll()
+        additional.roll()
 
         swapper.cheat()
         loaded_dice.cheat()
+        mulligan.cheat()
+        additional.cheat()
         """Remove # before print statements to see simulation running
            Simulation takes approximately one hour to run with print
            statements or ten seconds with print statements
@@ -46,6 +55,14 @@ def main():
         else:
             #print("Loaded dice wins!")
             loaded_dice_score += 1
+
+        if sum(mulligan.get_dice()) == sum(additional.get_dice()):
+            pass
+        elif sum(mulligan.get_dice()) > sum(additional.get_dice()):
+            mulligan_score+= 1
+        else:
+            additional_score += 1
+        
         game_number += 1
 
     # the game has ended
@@ -55,7 +72,11 @@ def main():
     print("------------")
     print(f"Swapper won: {swapper_score}")
     print(f"Loaded dice won: {loaded_dice_score}")
-
+    print("------------")
+    print(f"Mulligan won: {mulligan_score}")
+    print(f"Additional won: {additional_score}")
+            
+    
     # determine the winner
     if swapper_score == loaded_dice_score:
         print("Game was drawn")
@@ -63,6 +84,13 @@ def main():
         print("Swapper won most games")
     else:
         print("Loaded dice won most games")
+    
+    if mulligan_score == additional_score:
+        print("Game2 was drawn")
+    elif mulligan_score > additional_score:
+        print("Mulligan won most games")
+    else:
+        print("Additional won most games")
 
 if __name__ == "__main__":
     main()
